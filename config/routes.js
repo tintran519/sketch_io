d5f868a03b6e1ad1fe46154b7d89e6612ade15f5
Roy Hwang <tupaclypse@gmail.com>
2016-11-21 19:35:54 -0800

    RESTful API done

--- config/routes.js
+++ config/routes.js
@@ -37,10 +37,25 @@ router.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-
-
 // users resource paths:
 router.get('/users',     usersController.index);
 router.get('/users/:id', usersController.show);
+
+//RESTful API- Create User Route (Roy)
+router.post('/api/users', usersController.create);
+//RESTful API- Show All Users (Roy)
+router.get('/api/users', usersController.indexAPI);
+//RESTful API- Show one user (Roy)
+router.get('/api/users/:id', usersController.showAPI);
+//RESTful API- Delete a user (Roy)
+router.delete('/api/users/:id', usersController.destroy);
+//RESTful API- Update a user info (Roy)
+router.put('/api/users/:id', usersController.update);
+
+//testing /api path (Roy)
+router.get('/api', function(req, res, next){
+  res.json({ message: "Sending JSON!"});
+});
+
 
 module.exports = router;
